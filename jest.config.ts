e7d6fcc75e8b98436723878c60a23fa62b8bc415
João Vitor Duarte Mariucio <joaovitorduartemariucio@gmail.com>
2024-07-04 12:00:04 -0300

    feat: fix from run tests in vscode

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,14 @@
-import type { Config } from 'jest';
+import type { Config } from '@jest/types';
 
-const config: Config = {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  rootDir: './',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
 };
 
 export default config;
